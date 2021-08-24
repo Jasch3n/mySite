c7e764e0b115ddfed5f7ea14cf3e12058feddefe
jaspaaaaaa <jasper20020202@gmail.com>
2021-08-24 14:54:44 -0700

    deployment adapation

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,12 +1,26 @@
 const template = document.createElement('sidebar');
-template.innerHTML = `
-<div>
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a class="navitem" href="/index.html">home</a> 
-    <a class="navitem" href="/static/writings.html">writings</a>
-    <a class="navitem" href="/static/poetry.html">poetry</a>
-</div>
-`
+const IS_ONLINE = true
+
+if (IS_ONLINE) {
+    template.innerHTML = `
+    <div>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a class="navitem" href="mySite/index.html">home</a> 
+        <a class="navitem" href="mySite/static/writings.html">writings</a>
+        <a class="navitem" href="mySite/static/poetry.html">poetry</a>
+    </div>
+    `
+} else {
+    template.innerHTML = `
+    <div>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a class="navitem" href="/index.html">home</a> 
+        <a class="navitem" href="/static/writings.html">writings</a>
+        <a class="navitem" href="/static/poetry.html">poetry</a>
+    </div>
+    `
+}
+
 let sidebarNode = document.getElementById("side-bar")
 
 sidebarNode.appendChild(template);
