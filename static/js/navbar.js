bba6079cea8c1d4a6fd51018e4873f0cce5d771a
jaspaaaaaa <jasper20020202@gmail.com>
2024-06-03 20:05:06 -0700

    navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,15 +1,6 @@
 const template = document.createElement('sidebar');
 const IS_ONLINE = true
 
-template.innerHTML = `
-    <div>
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a class="navitem" href="/mySite/index.html">home</a> 
-        <a class="navitem" href="/mySite/static/writings.html">writings</a>
-        <a class="navitem" href="/mySite/static/poetry.html">poetry</a>
-    </div>
-    `
-
 if (IS_ONLINE) {
     console.log("Website is in online version")
     template.innerHTML = `
